4904d01a6880a9775d7baee9c3e0e0625d3e2156
Ajiwunmi <taiwo.it@gmail.com>
2022-11-30 03:33:08 +0100

    Pycodestyle validation error fixed

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -4,8 +4,8 @@ import random
 number = random.randint(-10, 10)
 # YOUR CODE HERE
 if number > 0:
-    print(f'{number} is positive')
+    print(f'{number} is positive ')
 elif number == 0:
-    print(f'{number} is zero')
+    print(f'{number} is zero ')
 else:
-    print(f'{number} is negative\n')
+    print(f'{number} is negative ')
