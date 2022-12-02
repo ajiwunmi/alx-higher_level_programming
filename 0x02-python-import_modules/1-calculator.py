d6d490b57f4fa405592a850a438d68a3bd52f37a
Ajiwunmi <taiwo.it@gmail.com>
2022-12-02 04:17:34 +0100

    1-calculator file updated

--- 0x02-python-import_modules/1-calculator.py
+++ 0x02-python-import_modules/1-calculator.py
@@ -8,5 +8,6 @@ if __name__ == "__name__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
+    
 
 
