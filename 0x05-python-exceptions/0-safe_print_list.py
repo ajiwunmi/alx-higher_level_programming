f2da868d3c04cd3f695cf0b9fa1c31d0fa7bc48f
Ajiwunmi <ajiwunmi@gmail.com>
2022-12-19 20:26:02 -0800

    code doc added

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    """function prints x elements of a list
+    Args :
+        my_list(list): list of any type (integer, string, etc)
+        x(int) : number of elements to be printed
+    Returns
+        the number of element printed
+    """
     try:
         i = 0
         while i < x:
