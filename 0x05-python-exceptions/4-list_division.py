f6b8829f2a5d523d02dbd8a8ff68828bf47ff8c1
Ajiwunmi <taiwo.it@gmail.com>
2022-12-20 17:09:21 +0100

    bug fixed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -20,7 +20,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print('division by 0')
             div = 0
         except IndexError:
-            print('Out of range')
+            print('out of range')
             div = 0
         finally:
             new_list.append(div)
