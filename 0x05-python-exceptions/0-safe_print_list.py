4142a5660e823f1723dc6ca3aed138ee93c6be81
Ajiwunmi <ajiwunmi@gmail.com>
2022-12-20 00:52:22 -0800

    print format updated

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -12,7 +12,7 @@ def safe_print_list(my_list=[], x=0):
     for n in range(x):
         try:
 
-            print("{:d}".format(my_list[n]), end="")
+            print("{}".format(my_list[n]), end="")
             count += 1
         except IndexError:
             break
