7b365fa41cf9e4a020f02e52199cfdf932a040c4
Ajiwunmi <taiwo.it@gmail.com>
2022-12-20 13:19:18 +0100

    hot fix

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -12,9 +12,7 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             i += 1
-        except (TypeError):
-            i -=1
-        except ValueError:
-            i -=1      
+        except (TypeError, ValueError):
+            i -=1    
     print()
     return (i)
