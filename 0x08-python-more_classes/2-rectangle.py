d2a0499b12ef99b6ea7e5b97e9d925cdd8ec67c2
Ajiwunmi <taiwo.it@gmail.com>
2023-01-03 12:48:57 +0100

    class attribute updated

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -47,4 +47,4 @@ class Rectangle:
         """Calculate the permiter of a rectangle"""
         if self.__width == 0 or self.__height == 0:
             return (0)
-        return (2 * (self.__height + self.__width))
+        return ((self.__width * 2) + (self.__height * 2))
