df43cbf11c29439aaa98bef02eba2d6125f23dda
Ajiwunmi <taiwo.it@gmail.com>
2023-04-26 21:03:29 +0100

    Text fomat standandized
    Signed-off-by: Ajiwunmi <taiwo.it@gmail.com>

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,6 +2,6 @@
 const fs = require("fs");
 fs.readFile(process.argv[2], "utf8", function (error, content) {
 	// console.log(error || content);
-	if (error) console.log(error);
+    if (error) console.log(error);
 	// else console.log(content);
 });
