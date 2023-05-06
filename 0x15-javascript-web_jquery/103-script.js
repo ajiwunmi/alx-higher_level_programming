72beb89ce514793bec6b0fdb50bba55cd54615c7
Ajiwunmi <taiwo.it@gmail.com>
2023-05-06 07:59:22 +0100

    updated the url on 102-script.js
    Signed-off-by: Ajiwunmi <taiwo.it@gmail.com>

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,17 +1,23 @@
-$('document').ready(function () {
-  $('INPUT#btn_translate').click(translate);
-  $('INPUT#language_code').focus(function () {
-    $(this).keydown(function (e) {
-      if (e.keyCode === 13) {
-        translate();
-      }
-    });
-  });
+$("document").ready(function () {
+	$("input#btn_translate").click(translate);
+	$("input#language_code").focus(function () {
+		$(this).keydown(function (e) {
+			if (e.keyCode === 13) {
+				translate();
+			}
+		});
+	});
 });
 
-function translate () {
-  const url = 'https://www.fourtonfish.com/hellosalut/?';
-  $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-    $('DIV#hello').html(data.hello);
-  });
+function translate() {
+	const url = "https://hellosalut.stefanbohacek.dev/?";
+	// const url = "https://www.fourtonfish.com/hellosalut/?";
+	// const url = "https://hellosalut.stefanbohacek.dev/?lang=ja";
+	$.get(
+		url + $.param({ lang: $("input#language_code").val() }),
+		// url,
+		function (data) {
+			$("DIV#hello").html(data.hello);
+		}
+	);
 }
